56468f229d628d8e720741f20901e4d9fcf3edcf
techie-sakshi <kumarisakshi273@gmail.com>
2025-05-23 10:56:28 +0530

    Added reminder email facility

--- app/page.js
+++ app/page.js
@@ -1,102 +1,7 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-
-
-// export default function Home() {
-//   return (
-//     <div className={styles.page}>
-//       <main className={styles.main}>
-//         <Image
-//           className={styles.logo}
-//           src="/next.svg"
-//           alt="Next.js logo"
-//           width={180}
-//           height={38}
-//           priority
-//         />
-//         <ol>
-//           <li>
-//             Get started by editing <code>app/page.js</code>.
-//           </li>
-//           <li>Save and see your changes instantly.</li>
-//         </ol>
-
-//         <div className={styles.ctas}>
-//           <a
-//             className={styles.primary}
-//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <Image
-//               className={styles.logo}
-//               src="/vercel.svg"
-//               alt="Vercel logomark"
-//               width={20}
-//               height={20}
-//             />
-//             Deploy now
-//           </a>
-//           <a
-//             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className={styles.secondary}
-//           >
-//             Read our docs
-//           </a>
-//         </div>
-//       </main>
-//       <footer className={styles.footer}>
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/file.svg"
-//             alt="File icon"
-//             width={16}
-//             height={16}
-//           />
-//           Learn
-//         </a>
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/window.svg"
-//             alt="Window icon"
-//             width={16}
-//             height={16}
-//           />
-//           Examples
-//         </a>
-//         <a
-//           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <Image
-//             aria-hidden
-//             src="/globe.svg"
-//             alt="Globe icon"
-//             width={16}
-//             height={16}
-//           />
-//           Go to nextjs.org →
-//         </a>
-//       </footer>
-//     </div>
-//   );
-// }
-// dashboard.js
 "use client";
 import '../app/globals.css';
+import emailjs from 'emailjs-com';
+import { doc, updateDoc } from "firebase/firestore";
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { auth, db } from "../lib/firebase";
@@ -141,6 +46,25 @@ export default function Dashboard() {
 
   const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
   const UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+  const sendUnlockEmail = (capsule) => {
+    emailjs.send(
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+      {
+        user_name: user.displayName || user.email,
+        // capsule_title: capsule.message,
+        unlock_date: capsule.unlockDate.toLocaleString(),
+        to_email: user.email,
+      },
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+    )
+      .then((res) => {
+        console.log("📨 Email sent!", res.text);
+      })
+      .catch((err) => {
+        console.error("❌ Email error:", err);
+      });
+  };
 
 
   useEffect(() => {
@@ -182,6 +106,15 @@ export default function Dashboard() {
     });
     return () => unsub();
   }, [user]);
+  useEffect(() => {
+    capsules.forEach((cap) => {
+      if (isUnlocked(cap) && !cap.reminderSent) {
+        sendUnlockEmail(cap);
+        const capsuleRef = doc(db, "capsules", cap.id);
+        updateDoc(capsuleRef, { reminderSent: true });
+      }
+    });
+  }, [capsules]);
 
   const handleLogout = async () => {
     await signOut(auth);
